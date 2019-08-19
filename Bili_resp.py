cb5bc4dc85f4b7c432d9754647f99fd58c2bbb96
萌狸 <alileptc@gmail.com>
2019-08-20 00:19:23 +0800

    add joke

--- Bili_resp.py
+++ Bili_resp.py
@@ -68,25 +68,58 @@ def ran_face():
 def ran_han():
   return random.choice(['(;¬_¬)', '(~_~;)', ' = =!', '╮(╯▽╰)╭', '(シ_ _)シ', '>_<', '(๑• _ •๑)'])
 
+# 神器，能从语料库找最接近字符串
+import difflib
+
 def zhineng_reply(atstr,atmid,oid,parent,root,uri,bid):
+
+    atstr = atstr.replace('@狸工智能 ', '', 1).replace('@狸工智能', '', 1)
 
     if len(re.findall(r'用法|指南|说明|帮助|关键词|(怎么|可以)(问|查)|你(.{0,2})家|help',atstr)) > 0:
         po_reply('目前支持的关键词有：'+summary_list+'……详细指南见：http://github.com/LePtC/BiliResp '+ran_face(),oid,parent,root,uri,bid)
 
     elif len(re.findall(r'复读',atstr)) > 0:
-        po_reply(atstr.replace('@狸工智能 ', '', 1).replace('@狸工智能', '', 1),oid,parent,root,uri,bid)
+        po_reply(atstr,oid,parent,root,uri,bid)
 
     elif len(re.findall(r'狸(.{0,3})叫|fox(.{0,3})say',atstr)) > 0:
         po_reply(random.choice(['嘤','嘤嘤嘤','嘤嘤嘤嘤嘤','大楚兴，陈胜王'])+ran_face(),oid,parent,root,uri,bid)
 
-    elif len(re.findall(r'卖(.{0,3})萌',atstr)) > 0:
+    elif len(re.findall(r'(卖|买)(.{0,3})萌',atstr)) > 0:
         po_reply('狸子'+random.choice(['敲','敲极'])+random.choice(['可','阔'])+'爱～'+ran_face(),oid,parent,root,uri,bid)
+
+    elif len(re.findall(r'(可|喜)爱|喜欢|萌|高兴|(快|欢)乐|愉快|幸福',atstr)) > 0:
+        po_reply('狐狸搓一搓，生活欢乐多～'+ran_face(),oid,parent,root,uri,bid)
 
     elif len(re.findall(r'你是|真人|自(.{0,3})介绍|介绍(.{0,4})自',atstr)) > 0:
         po_reply('我是狸子LePtC研发'+random.choice(['','时长两天半'])+'的虚拟UP主'+random.choice(['～','鸭'])+ran_face(),oid,parent,root,uri,bid)
 
+    elif len(re.findall(r'狸子(.{0,4})(生日|几岁|多大|出生|破壳)',atstr)) > 0:
+        po_reply('狸子是公元199东汉末年出生的狐狸～（生日连它自己都不记得辣）'+ran_face(),oid,parent,root,uri,bid)
+
+    elif len(re.findall(r'(你|狸工智能)(.{0,4})(生日|几岁|多大|出生|破壳)',atstr)) > 0:
+        po_reply('我是从2019年8月16号开始试运行'+random.choice(['的奥','哒'])+ran_face(),oid,parent,root,uri,bid)
+
+    elif len(re.findall(r'(?i)(LePtC|萌狸|狸(子|君|酱|神))(.{0,4})男|你(.{0,4})女',atstr)) > 0:
+        po_reply('狸子是男狐狸'+random.choice(['奥，','～'])+'其它保密 '+ran_face(),oid,parent,root,uri,bid)
+
+    elif len(re.findall(r'(你|狸工智能)(.{0,4})男|你(.{0,4})女',atstr)) > 0:
+        po_reply('我是AI，没有性别'+random.choice(['的奥','哒'])+ran_face(),oid,parent,root,uri,bid)
+
     elif len(re.findall(r'你会',atstr)) > 0:
         po_reply(random.choice(['','我'])+'现在只会卖萌'+ran_han(),oid,parent,root,uri,bid)
+
+    elif len(re.findall(r'笑话|段子|聊|唠嗑',atstr)) > 0:
+        with open(path2+'jokes.txt', 'r', encoding='utf-8') as file:
+            joke_list = [x.replace ("\n","") for x in file.readlines()]
+        close_jokes = difflib.get_close_matches(atstr.strip('讲一个笑话段子聊唠嗑啊的是了我你他们说人不在有这个上来到时为什么啥'), joke_list, 2, 0.01)
+        if len(close_jokes) > 0:
+            po_joke = random.choice(close_jokes)
+        else:
+            po_joke = random.choice(mid_list)
+        po_reply(po_joke,oid,parent,root,uri,bid)
+
+    elif len(re.findall(r'怕',atstr)) > 0:
+        po_reply('我最怕断电、断网、欠费停机…'+ran_han(),oid,parent,root,uri,bid)
 
     else:
         po_reply('（这条艾特中没有可回复的关键词'+random.choice(['～','奥'])+ran_han(),oid,parent,root,uri,bid)
